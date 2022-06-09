a5267b74d762a9004cb4557e4cf7470934d7e1e2
edwardz8 <edwardz8@yahoo.com>
2022-06-09 15:38:12 -0400

    init

---
+++ src/main.rs
@@ -0,0 +1,42 @@
+use std::io;
+use rusqlite::{Connection, Result};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let conn = Connection::open("notes.db")?;
+    conn.execute(
+        "create table if not exists notes(
+            id integer primary key,
+            body text not null unique
+        )",
+        [],
+    )?;
+
+    let mut running = true;
+    while running == true {
+        let mut buffer = String::new();
+        io::stdin().read_line(&mut buffer)?;
+
+        let trimmed_body = buffer.trim();
+        let cmd_split = trimmed_body.split(" ");
+
+        if trimmed_body == "" {
+            running = false;
+        } 
+        else if trimmed_body == "/list" {
+            let mut stmt = conn.prepare("SELECT id, body from notes")?;
+            let mut rows = stmt.query(rusqlite::params![])?;
+
+            while let Some(row) = rows.next()? {
+                let id: i32 = row.get(0)?;
+                let body: String = row.get(1)?;
+                println!("{} {}", id, body.to_string());    
+            }
+        }
+        else {
+            conn.execute("INSERT INTO notes (body) values (?1)", [trimmed_body])?;
+        }
+    }
+
+    Ok(())
+}
+
